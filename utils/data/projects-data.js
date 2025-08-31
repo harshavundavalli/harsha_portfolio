4b5f79bc8a96af989e8f4b5a8d9681260fd5ebe8
Harsha <harshachowdaryvundavalli1881@gmail.com>
2025-08-30 23:50:30 -0500

    updated_projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -17,17 +17,9 @@ export const projectsData = [
         code: '',
         demo: '',
     },
+    
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-    },
-    {
-        id: 4,
         name: 'Newsroom Management',
         description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
         tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
