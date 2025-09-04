95574c4e89c2d59396156340af5b4776b3aee36e
Harsha <harshachowdaryvundavalli1881@gmail.com>
2025-09-04 00:21:55 -0500

    made changes in education section

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -25,10 +25,10 @@ function AboutSection() {
         <div className="flex justify-center order-1 lg:order-2">
           <Image
             src={personalData.profile}
-            width={280}
+            width={370}
             height={280}
             alt="Harsha Vundavalli"
-            className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
+            className="rounded-lg transition-all duration-1000  hover:scale-125 cursor-pointer"
           />
         </div>
       </div>
--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,11 +4,13 @@ export const educations = [
     title: "M.S Computer Science",
     duration: "2024 - Present",
     institution: "University of Texas at Dallas",
+    description: "OOPS"
   },
   {
     id: 2,
     title: "B.E Computer Science & Engineering",
     duration: "2019 - 2023",
     institution: "Birla Institute of Technology & Science Pilani, Hyderabad Campus",
+    description:"OOPS"
   }
 ]
